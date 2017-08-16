beec7ebe328999aff0f1a5db0c48ce240d081f9e
autohome <day_clear.com>
2017-08-16 17:46:51 +0800

    无法找到socket，待解决

--- app.js
+++ app.js
@@ -1,14 +1,63 @@
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
+var http = require('http');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use('/js', express.static(path.resolve(__dirname, './build/js')));
-app.route('/')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extends: false}))
+app.route('/:id')
       .get(function(req, res) {
         var html = fs.readFileSync(path.resolve('./build/' + 'index.html'), 'utf-8');
         res.send(html);
       });
+
+var server = http.Server(app);
+var io = require('socket.io').listen(server);
+var roomUser = [];
+
+io.on('connection', function(socket) {
+  var url = socket.request.headers.referer;
+  var split_arr = url.split('/');
+  var roomid = split_arr[split_arr.length-1] || 'index';
+  var user = '';
+
+  socket.on('join', function(username) {
+    user = username;
+    // 将用户归类到房间
+    if (!roomUser[roomid]) {
+      roomUser[roomid] = [];
+    }
+    roomUser[roomid].push(user);
+    socket.join(roomid);
+    socket.to(roomid).emit('sys', user + '加入了房间');
+    socket.emit('sys', user + '加入了房间');
+  });
+
+  socket.on('message', function(mes) {
+    if (roomUser[roomid].indexOf(user)<0) {
+      return false;
+    }
+    socket.to(roomid).emit('newMessage', mes, user);
+    socket.emit('newMessage', mes, user);
+  });
+
+  socket.on('disconnect', function() {
+    socket.leave(roomid, function(err) {
+      if (err) {
+        log.error(err);
+      } else {
+        var index = roomUser[roomid].indexOf(user);
+        if (index !== -1) {
+          roomUser[roomid].splice(index, 1);
+          socket.to(roomid.emit('sys', user+'退出了房间'));
+        }
+      }
+    })
+  })
+})
 
 app.listen(8888, function() {
     console.log("应用实例，访问地址为 localhost:8888")
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,40 @@
 import '../style/style.scss';
+
+(window.onload = function() {
+
+  (function(){
+    var username = prompt('请输入昵称');
+    var nameSpan = document.getElementById('name');
+    var box = document.getElementById('box');
+    var input = document.getElementById('input');
+    var submit = document.getElementById('submit');
+    var socket = io();
+
+    nameSpan.innerHTML = username;
+    socket.addEventListener('connect', function() {
+      socket.emit('join', username);
+    });
+
+    socket.addEventListener('sys', function(mes) {
+      box.innerHTML += '<p>'+mes+'</p>';
+    });
+
+    socket.addEventListener('newMessage', function(mes, user) {
+      box.innerHTML += '<p>'+user+'说：'+msg+'</p>';
+    });
+
+    submit.addEventListener('click', function() {
+      var html = input.value;
+      if(!html || html === ''){
+        return false;
+      } else {
+        socket.send(html);
+      }
+    })
+
+    function scrollToBottom () {
+      box.scrollTop = box.scrollHeight;
+    }
+  }());
+
+}())
